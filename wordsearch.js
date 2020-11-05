0acd99003f830312412621601cadabd3fce9e847
EagerBeaver98 <jooke98@outlook.com>
2020-11-05 01:12:14 +0000

    program can check vertical and horizontal

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,21 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  const verticalFlip = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    verticalFlip[i] = [];
+    for (let k = 0; k < letters.length; k++) {
+      verticalFlip[i][k] = letters[k][i];
     }
-}
+  }
+  const verticalJoin = verticalFlip.map(ls => ls.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  return false;
+};
+
+module.exports = wordSearch;
